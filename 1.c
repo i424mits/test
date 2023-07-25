0d64588ad2f5ecc6a6837185aee859c6f05d3463
i424mits <140384307+i424mits@users.noreply.github.com>
2023-07-25 16:53:07 +0900

    1.c

--- 1.c
+++ 1.c
@@ -1,1 +1,13 @@
-a
+#include <stdio.h>
+
+int main(void){
+  int n;
+
+  printf("n = ");
+  scanf("%d", &n);
+
+  if ( ( n & 1 ) == 0 ){ /*()をつけることにより、n & 1 を先に計算することができるようになるため*/
+    printf("%d は偶数です\n", n);
+  }
+  return 0;
+}
